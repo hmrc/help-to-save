61483dd9e2414f205a0332e5f478bf07ceeb494a
Colm Cavanagh <colm.m.cavanagh@gmail.com>
2017-05-16 13:29:29 +0100

    Colm andy/hts 156/chat to nsi stub (#5)

---
+++ app/uk/gov/hmrc/helptosaveeligibilitycheck/connectors/NSIConnector.scala
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosaveeligibilitycheck.connectors
+
+import javax.inject.Singleton
+
+import com.google.common.base.Charsets
+import com.google.common.io.BaseEncoding
+import com.google.inject.ImplementedBy
+import play.api.Logger
+import play.api.http.Status
+import play.api.libs.json.{Format, JsError, JsSuccess, Json}
+import uk.gov.hmrc.helptosaveeligibilitycheck.WSHttp
+import uk.gov.hmrc.helptosaveeligibilitycheck.connectors.NSIConnector.{SubmissionFailure, SubmissionResult, SubmissionSuccess}
+import uk.gov.hmrc.helptosaveeligibilitycheck.models.NSIUserInfo
+import uk.gov.hmrc.play.config.ServicesConfig
+import uk.gov.hmrc.play.http.ws.WSPost
+import uk.gov.hmrc.play.http.{HeaderCarrier, HttpResponse}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@ImplementedBy(classOf[NSIConnectorImpl])
+trait NSIConnector {
+  def createAccount(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[SubmissionResult]
+}
+
+object NSIConnector {
+
+  sealed trait SubmissionResult
+
+  case object SubmissionSuccess extends SubmissionResult
+
+  case class SubmissionFailure(errorMessageId: Option[String], errorMessage: String, errorDetail: String) extends SubmissionResult
+
+  implicit val submissionFailureFormat: Format[SubmissionFailure] = Json.format[SubmissionFailure]
+
+}
+
+@Singleton
+class NSIConnectorImpl extends NSIConnector with ServicesConfig {
+
+  val nsiUrl: String = baseUrl("nsi")
+  val nsiUrlEnd: String = getString("microservice.services.nsi.url")
+  val url = s"$nsiUrl/$nsiUrlEnd"
+  val encodedAuthorisation: String = {
+    val userName: String = getString("microservice.services.nsi.username")
+    val password: String = getString("microservice.services.nsi.password")
+    BaseEncoding.base64().encode((userName + ":" + password).getBytes(Charsets.UTF_8))
+  }
+
+  val http: WSPost = WSHttp
+
+  override def createAccount(userInfo: NSIUserInfo)(implicit hc: HeaderCarrier, ex: ExecutionContext): Future[SubmissionResult] = {
+    Logger.debug("We are posted to NSI " + url + " with data " + userInfo)
+    http.POST[NSIUserInfo, HttpResponse](url, userInfo,
+      headers = Seq(("Authorization", encodedAuthorisation))).map { response =>
+      response.status match {
+        case Status.CREATED ⇒
+          Logger.debug("We have successfully created a NSI account")
+          SubmissionSuccess
+        case Status.BAD_REQUEST ⇒
+          Logger.error("We have failed to make an account due to a bad request")
+          Json.fromJson[SubmissionFailure](response.json) match {
+            case JsSuccess(failure, _) ⇒ failure
+            case e: JsError ⇒ SubmissionFailure(None, s"Could not create NSI account errors", e.errors.toString())
+          }
+        case other ⇒
+          Logger.warn("Something went wrong nsi ")
+          SubmissionFailure(None, s"Bad Status", other.toString)
+      }
+    }
+  }
+}
---
+++ app/uk/gov/hmrc/helptosaveeligibilitycheck/controllers/NSIController.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.helptosaveeligibilitycheck.controllers
+
+import com.google.inject.Inject
+import play.api.Logger
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.{JsError, JsSuccess, Json}
+import play.api.mvc.{Action, AnyContent}
+import uk.gov.hmrc.helptosaveeligibilitycheck.connectors.NSIConnector
+import uk.gov.hmrc.helptosaveeligibilitycheck.connectors.NSIConnector.{SubmissionFailure, SubmissionSuccess}
+import uk.gov.hmrc.helptosaveeligibilitycheck.models.{NSIUserInfo, UserInfo}
+import uk.gov.hmrc.play.microservice.controller.BaseController
+
+import scala.concurrent.Future
+
+class NSIController @Inject()(nsiConnector: NSIConnector) extends BaseController {
+
+  def createAccount: Action[AnyContent] = Action.async { implicit request =>
+    request.body.asJson.map(_.validate[UserInfo]) match {
+      case None ⇒
+        Logger.error("We have failed to create an account due to having No Json ")
+        Future.successful(BadRequest(
+          Json.toJson(SubmissionFailure(None, "No json", ""))))
+      case Some(er: JsError) ⇒
+        Logger.error("We have failed to create an account due to invalid Json " + er.toString)
+        Future.successful(BadRequest(
+          Json.toJson(SubmissionFailure(None, "Invalid Json", er.errors.toString()))))
+      case Some(JsSuccess(createAccount, _)) ⇒
+        NSIUserInfo(createAccount).fold(
+          errors ⇒ {
+            Logger.info("We have failed to create an account due to invalid user details " + errors)
+            Future.successful(BadRequest(
+              Json.toJson(SubmissionFailure(None, "Invalid user details", errors.toList.mkString(",")))))
+          },
+          nSIUserInfo ⇒ {
+            nsiConnector.createAccount(nSIUserInfo).flatMap {
+              _ match {
+                case SubmissionSuccess ⇒
+                  Logger.debug("We have created an account on NSI ")
+                  Future.successful(Created)
+                case SubmissionFailure(_, em, ed) ⇒
+                  Logger.error(s"We have failure to create an account on nsi because $em and $ed")
+                  Future.successful(BadRequest)
+              }
+            }
+          }
+        )
+    }
+  }
+}
