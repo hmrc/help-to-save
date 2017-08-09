fea229d1781d07c9bc266b77fb06a55d2d0825c7
Jacqueline Hubbard <Jackie1988@users.noreply.github.com>
2017-08-09 16:31:02 +0100

    Hts 330 (#27) MissingMandatoryData integration test

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -29,7 +29,8 @@ object MicroServiceBuild extends Build with MicroService {
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "com.github.tomakehurst" % "wiremock" % "2.5.1" % scope,
     "org.scalamock" %% "scalamock-scalatest-support" % "3.5.0" % scope,
-    "hmrc" %% "stub-data-generator" % "0.2.0" % scope
+    "hmrc" %% "stub-data-generator" % "0.2.0" % scope,
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope
   )
 
 }
