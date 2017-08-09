72da639d61fa8d96491c417b6c8538395e544870
Andy <andrew.chung@digital.hmrc.gov.uk>
2017-08-09 12:02:53 +0100

    Tidying up

--- it/uk/gov/hmrc/helptosave/MissingMandatoryData.scala
+++ it/uk/gov/hmrc/helptosave/MissingMandatoryData.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.hmrc.helptosave
 
 import java.net.URLEncoder
@@ -21,35 +36,33 @@ import uk.gov.hmrc.helptosave.services.UserInfoService.UserInfoServiceError.Miss
 import uk.gov.hmrc.helptosave.support.WiremockSupport
 import uk.gov.hmrc.helptosave.util.NINO
 
-
 class MissingMandatoryData extends WordSpec
   with Matchers
   with ScalaFutures
   with WiremockSupport
   with BeforeAndAfterAll
   with BeforeAndAfterEach
- with GuiceOneServerPerSuite {
+  with GuiceOneServerPerSuite {
 
-  implicit override val patienceConfig = PatienceConfig(timeout = Span(5, Seconds), interval = Span(100, Millis))
+  implicit override val patienceConfig =
+    PatienceConfig(timeout = Span(5, Seconds), interval = Span(100, Millis))
 
   override lazy val port: Int = 7001
 
   val wsClient = app.injector.instanceOf[WSClient]
 
-  val url = "http://localhost:7001/help-to-save/eligibility-check"
+  val eligibilityCheckURL = "http://localhost:7001/help-to-save/eligibility-check"
 
-  def decode(encodedNINO: String): NINO = {
+  def decode(encodedNINO: String): NINO =
     new String(Base64.getDecoder.decode(encodedNINO))
-  }
 
-  def checkEligibility(encodedNino: String, userDetailsURI: String): WSResponse = {
+  def checkEligibility(encodedNino: String, userDetailsURI: String): WSResponse =
     wsClient
-      .url(s"$url?nino=${decode(encodedNino)}&userDetailsURI=${URLEncoder.encode(userDetailsURI)}")
+      .url(s"$eligibilityCheckURL?nino=${decode(encodedNino)}&userDetailsURI=${URLEncoder.encode(userDetailsURI)}")
       .get()
       .futureValue
-  }
 
-  def stubCall(url: String, json: String) = {
+  def stubCall(url: String, json: String): Unit =
     wireMockServer.stubFor(
       get(urlPathMatching(url))
         .willReturn(
@@ -58,7 +71,17 @@ class MissingMandatoryData extends WordSpec
             .withBody(json)
         )
     )
-  }
+
+  def stubEligibilityCheck(result: Boolean): Unit =
+    stubCall("/help-to-save-stub/eligibilitycheck.*", s"""{ "isEligible" : $result}""")
+
+  def stubUserDetails(userDetailsResponse: UserDetailsResponse): Unit =
+    stubCall("/user-details.*", Json.toJson(userDetailsResponse).toString())
+
+  def stubCitizenDetails(citizenDetailsResponse: CitizenDetailsResponse): Unit =
+    stubCall(".*/citizen-details.*", Json.toJson(citizenDetailsResponse).toString)
+
+
 
   // To do: convert to Gherkin/Cucumber
   "Checking if a user can create an account" when {
@@ -77,11 +100,11 @@ class MissingMandatoryData extends WordSpec
           val expected: UserInfo = UserInfo("Sarah", "Smith", decode(encodedNino), LocalDate.of(1999, 12, 12), "email@gmail.com",
             Address(List("line 1", "line 2", "line 3", "line 4", "line 5"), Some("BN43 XXX"), Some("GB")))
 
-          stubCall("/help-to-save-stub/eligibilitycheck.*", """{ "isEligible" : true }""")
-          stubCall("/user-details.*", Json.toJson(userDetails).toString())
-          stubCall(".*/citizen-details.*", Json.toJson(citizenDetailsResponse).toString)
+          stubEligibilityCheck(true)
+          stubUserDetails(userDetails)
+          stubCitizenDetails(citizenDetailsResponse)
 
-          val checkEligibilityResponse: WSResponse = checkEligibility(encodedNino, "http://localhost:7002/user-details/hello")
+          val checkEligibilityResponse: WSResponse = checkEligibility(encodedNino, s"http://$host:$port/user-details/$encodedNino")
 
           checkEligibilityResponse.status shouldBe OK
           val json = Json.toJson(expected)
@@ -89,9 +112,7 @@ class MissingMandatoryData extends WordSpec
         }
       }
     }
-
   }
-
 
   "Checking an applicant cannot create an account" when {
 
@@ -105,11 +126,11 @@ class MissingMandatoryData extends WordSpec
         val citizenDetailsResponse = CitizenDetailsResponse(Some(citizenDetailsPerson), Some(citizenDetailsAddress))
         val userDetails = UserDetailsResponse("Sarah", None, Some("email@gmail.com"), Some(LocalDate.of(1999, 12, 12)))
 
-        stubCall("/help-to-save-stub/eligibilitycheck.*", """{ "isEligible" : true }""")
-        stubCall("/user-details.*", Json.toJson(userDetails).toString())
-        stubCall(".*/citizen-details.*", Json.toJson(citizenDetailsResponse).toString)
+        stubEligibilityCheck(true)
+        stubUserDetails(userDetails)
+        stubCitizenDetails(citizenDetailsResponse)
 
-        val checkEligibilityResponse: WSResponse = checkEligibility(encodedNino, "http://localhost:7002/user-details/")
+        val checkEligibilityResponse: WSResponse = checkEligibility(encodedNino, s"http://$host:$port/user-details/$encodedNino")
 
         checkEligibilityResponse.status shouldBe OK
         val missingUserInfos = MissingUserInfos(Set(Surname))
@@ -131,16 +152,16 @@ class MissingMandatoryData extends WordSpec
         val citizenDetailsResponse = CitizenDetailsResponse(Some(citizenDetailsPerson), Some(citizenDetailsAddress))
         val userDetails = UserDetailsResponse("Sarah", None, None, Some(LocalDate.of(1999,12,12)))
 
-        stubCall("/help-to-save-stub/eligibilitycheck.*", """{ "isEligible" : true }""")
-        stubCall("/user-details.*", Json.toJson(userDetails).toString())
-        stubCall(".*/citizen-details.*", Json.toJson(citizenDetailsResponse).toString)
+        stubEligibilityCheck(true)
+        stubUserDetails(userDetails)
+        stubCitizenDetails(citizenDetailsResponse)
 
-        val checkEligibilityResponse: WSResponse = checkEligibility(encodedNino, "http://localhost:7002/user-details/")
+        val checkEligibilityResponse: WSResponse = checkEligibility(encodedNino, s"http://$host:$port/user-details/hello")
         checkEligibilityResponse.status shouldBe OK
         val missingUserInfos = MissingUserInfos(Set(Email))
         checkEligibilityResponse.json shouldBe Json.parse("""{"result":{"missingInfo":["Email"]}}""")
-        }
       }
     }
+  }
 
 }
--- it/uk/gov/hmrc/helptosave/support/WiremockSupport.scala
+++ it/uk/gov/hmrc/helptosave/support/WiremockSupport.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.gov.hmrc.helptosave.support
 
 import com.github.tomakehurst.wiremock.WireMockServer
@@ -6,17 +21,17 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, Suite}
 
-trait WiremockSupport extends BeforeAndAfterAll with BeforeAndAfterEach with ScalaFutures {
-// To do: Decide whether to use stubs here or stub repo - we don't want to duplicate stubs for Selenium tests
-  this: Suite =>
+trait WiremockSupport extends BeforeAndAfterAll with BeforeAndAfterEach {
+  // To do: Decide whether to use stubs here or stub repo - we don't want to duplicate stubs for Selenium tests
+  this: Suite ⇒
 
-  val Host = "http://localhost"
-  val Port = 7002
-  lazy val wireMockServer = new WireMockServer(wireMockConfig().port(Port))
+  val host = "http://localhost"
+  val port = 7002
+  lazy val wireMockServer = new WireMockServer(wireMockConfig().port(port))
 
   override def beforeAll() = {
     super.beforeAll()
-    WireMock.configureFor(Host, Port)
+    WireMock.configureFor(host, port)
     wireMockServer.start()
   }
 
@@ -26,7 +41,6 @@ trait WiremockSupport extends BeforeAndAfterAll with BeforeAndAfterEach with Sca
   }
 
   override def afterAll() = {
-    println("Stopping the mock backend server")
     super.afterAll()
     wireMockServer.stop()
   }
