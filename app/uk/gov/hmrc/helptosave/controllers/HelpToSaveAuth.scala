88c5f94a700e624480a7005d2363fd3efdbfb0e1
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2017-09-25 22:35:14 +0100

    cleanup

--- app/uk/gov/hmrc/helptosave/controllers/HelpToSaveAuth.scala
+++ app/uk/gov/hmrc/helptosave/controllers/HelpToSaveAuth.scala
@@ -23,12 +23,12 @@ import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.auth.core.authorise.Predicate
 import uk.gov.hmrc.auth.core.retrieve.Retrievals
 import uk.gov.hmrc.helptosave.config.HtsAuthConnector
-import uk.gov.hmrc.helptosave.util.toFuture
+import uk.gov.hmrc.helptosave.util.{Logging, toFuture}
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 import scala.concurrent.Future
 
-class HelpToSaveAuth(htsAuthConnector: HtsAuthConnector) extends BaseController with AuthorisedFunctions {
+class HelpToSaveAuth(htsAuthConnector: HtsAuthConnector) extends BaseController with AuthorisedFunctions with Logging {
 
   override def authConnector: AuthConnector = htsAuthConnector
 
@@ -45,18 +45,31 @@ class HelpToSaveAuth(htsAuthConnector: HtsAuthConnector) extends BaseController 
       authorised(AuthWithCL200)
         .retrieve(Retrievals.authorisedEnrolments) { authorisedEnrols ⇒
 
-            val mayBeNino = authorisedEnrols.enrolments
-              .find(_.key == "HMRC-NI")
-              .flatMap(_.getIdentifier("NINO"))
-              .map(_.value)
+          val mayBeNino = authorisedEnrols.enrolments
+            .find(_.key == "HMRC-NI")
+            .flatMap(_.getIdentifier("NINO"))
+            .map(_.value)
 
-            mayBeNino.fold(
-              toFuture(InternalServerError("could not find NINO for logged in user"))
-            )(_ ⇒ action(request))
-        }.recoverWith {
-        case e =>
-          toFuture(InternalServerError(s"Error during check auth: ${e.getMessage}"))
+          mayBeNino.fold(
+            toFuture(InternalServerError("could not find NINO for logged in user"))
+          )(_ ⇒ action(request))
+        }.recover {
+        handleFailure()
       }
     }
+
+  def handleFailure(): PartialFunction[Throwable, Result] = {
+    case _: NoActiveSession ⇒
+      logger.warn("user is not logged in, probably hack? ")
+      InternalServerError("")
+
+    case _: InsufficientConfidenceLevel | _: InsufficientEnrolments ⇒
+      logger.warn("unexpected: not met required ConfidenceLevel for logged in user")
+      InternalServerError("")
+
+    case ex: AuthorisationException ⇒
+      logger.warn(s"could not authenticate user due to: $ex")
+      InternalServerError("")
+  }
 }
 
