4db6a76841250af2018314276234930692438c37
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-01-05 12:25:14 +0000

    Always log DES response as requested by Tim (#74)

--- app/uk/gov/hmrc/helptosave/connectors/EligibilityCheckConnector.scala
+++ app/uk/gov/hmrc/helptosave/connectors/EligibilityCheckConnector.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -60,6 +60,8 @@ class EligibilityCheckConnectorImpl @Inject() (http:              WSHttp,
         http.get(url(nino), desHeaders)(hc.copy(authorization = None), ec)
           .map { response ⇒
             val time = timerContext.stop()
+
+            logger.info(s"eligibility response body from DES is: ${response.body}", nino)
 
             val res: Option[Either[String, EligibilityCheckResult]] = response.status match {
               case Status.OK ⇒
