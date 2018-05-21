f92a34c937ee68371088c30a282d645d599f5b3a
Suresh Mandalapu <suresh.mandalapu@digital.hmrc.gov.uk>
2018-05-21 14:46:50 +0100

    PR F - 2

--- app/uk/gov/hmrc/helptosave/repo/UCThresholdStore.scala
+++ app/uk/gov/hmrc/helptosave/repo/UCThresholdStore.scala
@@ -29,7 +29,6 @@ import uk.gov.hmrc.mongo.json.ReactiveMongoFormats
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.control.NonFatal
-import scala.util.{Failure, Success, Try}
 
 @ImplementedBy(classOf[MongoThresholdStore])
 trait ThresholdStore {
@@ -66,25 +65,33 @@ class MongoThresholdStore @Inject() (mongo: ReactiveMongoComponent)(implicit tra
 
   def getUCThreshold()(implicit ec: ExecutionContext): EitherT[Future, String, Option[Double]] =
     EitherT[Future, String, Option[Double]]({
-      findAll().map { res ⇒
-        Right(res.headOption
-          .map(data ⇒ data.thresholdAmount))
-      }.recover {
-        case e ⇒
-          Left(s"Could not read UC threshold value from threshold store: ${e.getMessage}")
-      }
+      tryFuture(
+        findAll().map { res ⇒
+          Right(res.headOption
+            .map(data ⇒ data.thresholdAmount))
+        }.recover {
+          case e ⇒
+            Left(s"Could not read UC threshold value from threshold store: ${e.getMessage}")
+        }
+      )
     })
 
   private[repo] def doUpdate(amount: Double)(implicit ec: ExecutionContext): Future[Option[UCThreshold]] = {
-    try {
+    tryFuture(
       collection.findAndUpdate(
         BSONDocument(),
         BSONDocument("$set" -> BSONDocument("thresholdAmount" -> amount)),
         fetchNewObject = true,
         upsert         = true
       ).map(_.result[UCThreshold])
+    )
+  }
+
+  def tryFuture[A](fa: ⇒ Future[A]): Future[A] = {
+    try {
+      fa
     } catch {
-      case e ⇒ Future.failed(e)
+      case NonFatal(e) ⇒ Future.failed(e)
     }
   }
 }
